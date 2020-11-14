193cb0833fafda44ea5045e3f071c22c64a43375
unknown <shashen008he@gmail.com>
2020-11-15 02:37:52 +0800

    2_3

---
+++ concurrency_pieces/2_3.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+void some_func();
+void some_other_func();
+
+struct func {
+    int &i;
+    func(int &i_) : i(i_) { }
+    void operator() () {
+        for (unsigned j = 0; j < 10; j++)  {
+            cout << "func: " << i + j << endl;
+        }
+    }
+};
+
+void f1() {
+    thread t1(some_func);
+    thread t2 = move(t1); // t1->t2, some_func
+    t1 = thread(some_other_func); // t1, some_other_func
+    thread t3;
+    t3 = move(t2); // t2->t3, some_func
+}
+
+// 同thread_guard不同在于，scoped_thread拥有线程的所有权
+class scoped_thread {
+    thread t;
+    public:
+    explicit scoped_thread(thread t_): t(move(t_)) {
+        if (!t.joinable()) {
+            throw logic_error("No thread");
+        }
+    }
+
+    ~scoped_thread() {
+        t.join();
+    }
+
+    scoped_thread(scoped_thread const &) = delete;
+    scoped_thread& operator=(scoped_thread const &) = delete;
+};
+
+void f2() {
+    int some_local_state = 1024;
+    scoped_thread t(thread(func(some_local_state)));
+    cout << "stupid f2" << endl;
+}
+
+void do_work(unsigned id) {
+    cout << "do work: " <<  id << endl;
+}
+
+void f3() {
+    vector<thread> threads; // vector支持移动操作
+    for (unsigned i = 0; i < 20; i++) {
+        threads.push_back(thread(do_work, i)); // move constructor
+    }
+
+    for_each(threads.begin(), threads.end(), mem_fn(&thread::join)); // call joins
+}
+
+int main() {
+
+    f1();
+
+    f2();
+
+    f3();
+
+    return 0;
+}
--- concurrency_pieces/README.md
+++ concurrency_pieces/README.md
@@ -1,16 +1,14 @@
 # Boring Concurrency Code
 
-*2020.11.14 --- Hello Concurrency World*
+## *2020.11.14 --- Hello Concurrency World*
 
-C++ 11 ---> \<thread>
-
-start thread = create thread object
-
-joined vs detached
-
+C++ 11 ---> \<thread>  
+start thread = create thread object  
+joined vs detached  
 pass params to thread func
 1. 默认参数会拷贝，即使参数为引用，避免参数为悬垂指针
 2. 使用std::ref转换为引用形式
 3. 可传递成员函数指针作为线程函数，并提供对象指针作为第一个参数
 4. 使用std::move转移动态对象到线程中去
+新线程所有权都要转移, thread 可移动，不可拷贝
 
