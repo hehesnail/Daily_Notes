717f6409c80339fc7a088c539d0172c49de65ccb
hehesnail <shashen008he@gmail.com>
2020-12-06 04:04:14 +0800

    update to 10.0 api

--- llvm_pieces/add_ir.cc
+++ llvm_pieces/add_ir.cc
@@ -1,20 +1,24 @@
 #include "llvm/ADT/SmallVector.h"
-#include "llvm/Analysis/Verifier.h"
+#include "llvm/IR/Verifier.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/CallingConv.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
-#include "llvm/Bitcode/ReaderWriter.h"
+#include "llvm/Bitcode/BitcodeReader.h"
+#include "llvm/Bitcode/BitcodeWriter.h"
 #include "llvm/Support/ToolOutputFile.h"
+#include "llvm-c/Core.h"
+#include "llvm/Support/raw_os_ostream.h"
+#include <iostream>
 
 using namespace llvm;
 using namespace std;
 
 Module *makeLLVMModule() {
     // First, Create the module, set datalayout and target triple
-    Module *mod = new Module("sum.ll", *LLVMGetGlobalContext());
+    Module *mod = new Module("sum.ll", *unwrap(LLVMGetGlobalContext()));
     mod->setDataLayout("e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128");
     mod->setTargetTriple("x86_64-unknown-linux-gnu");
 
@@ -39,20 +43,19 @@ Module *makeLLVMModule() {
     BasicBlock *labelEntry = BasicBlock::Create(mod->getContext(), "", funcSum, 0);
 
     // Instructions
-    AllocaInst *ptrA = new AllocaInst(IntegerType::get(mod->getContext(), 32), "3", labelEntry);
-    ptrA->setAlignment(4);
-    AllocaInst *ptrB = new AllocaInst(IntegerType::get(mod->getContext(), 32), "4", labelEntry);
-    ptrB->setAlignment(4);
+    AllocaInst *ptrA = new AllocaInst(IntegerType::get(mod->getContext(), 32), 0, "3", labelEntry);
+    ptrA->setAlignment(MaybeAlign(4));
+    AllocaInst *ptrB = new AllocaInst(IntegerType::get(mod->getContext(), 32), 0, "4", labelEntry);
+    ptrB->setAlignment(MaybeAlign(4));
 
     StoreInst *st0 = new StoreInst(int32_0, ptrA, false, labelEntry);
-    st0->setAlignment(4);
+    st0->setAlignment(MaybeAlign(4));
     StoreInst *st1 = new StoreInst(int32_1, ptrB, false, labelEntry);
-    st1->setAlignment(4);
-
+    st1->setAlignment(MaybeAlign(4));
     LoadInst *ld0 = new LoadInst(ptrA, "", false, labelEntry);
-    ld0->setAlignment(4);
+    ld0->setAlignment(MaybeAlign(4));
     LoadInst *ld1 = new LoadInst(ptrB, "", false, labelEntry);
-    ld1->setAlignment(4);
+    ld1->setAlignment(MaybeAlign(4));
 
     BinaryOperator *addRes = BinaryOperator::Create(Instruction::Add, ld0, ld1, "add", labelEntry);
     ReturnInst::Create(mod->getContext(), addRes, labelEntry);
@@ -62,16 +65,14 @@ Module *makeLLVMModule() {
 
 int main() {
     Module *Mod = makeLLVMModule();
-    verifyModule(*Mod, PrintMessage);
-    string ErrorInfo;
-    OwningAtomPtr<tool_output_file> Out(new tool_output_file("./sum.bc", ErrorInfo, sys::fs::F_None));
+    auto out_stream = new raw_os_ostream(cout);
+    verifyModule(*Mod, out_stream);
+    error_code ErrorInfo;
+    auto Out = new ToolOutputFile("./sum.bc", ErrorInfo, sys::fs::F_None);
     
-    if (!ErrorInfo.empty()) {
-        errs() << ErrorInfo << '\n';
-        return -1;
-    }
-
-    WriteBitcodeToFile(Mod, Out->os());
+    cout << Mod->getTargetTriple() << endl;
+    cout << Mod->getInstructionCount() << endl;
+    WriteBitcodeToFile(*Mod, Out->os());
     Out->keep();
 
     return 0;
