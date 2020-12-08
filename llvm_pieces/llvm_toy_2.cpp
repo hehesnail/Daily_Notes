f550af39950aa56b79f96505acc5e2f644fa06ba
unknown <shashen008he@gmail.com>
2020-12-09 02:13:25 +0800

    part llvm kaleidoscope ch2

---
+++ llvm_pieces/llvm_toy_2.cpp
@@ -0,0 +1,333 @@
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+/* The lexer*/
+enum Token {
+    tok_eof = -1, 
+
+    // commonds
+    tok_def = -2,
+    tok_extern = -3,
+
+    // primary
+    tok_identifier = -4,
+    tok_number = -5 
+};
+
+static string IdentifierStr; // Filled in if tok_identifier
+static double NumVal; //Filled in if tok_number
+
+// gettok - Return the next token from stantdard input.  
+static int gettok() {
+    static int LastChar = ' ';
+    
+    // skip the whitespace
+    while (isspace(LastChar)) {
+        LastChar = getchar();
+    }
+
+    // identifier: [a-zA-Z][a-zA-Z0-9]*   
+    if (isalpha(LastChar)) { 
+        IdentifierStr = LastChar;
+        while(isalnum((LastChar = getchar()))) {
+            IdentifierStr += LastChar;
+        }
+
+        if (IdentifierStr == "def") {
+            return tok_def;
+        }
+        if (IdentifierStr == "extern") {
+            return tok_extern;
+        }
+
+        return tok_identifier;
+    }
+
+    // Number: [0-9.]+
+    if (isdigit(LastChar) || LastChar == '.') {
+        string NumStr;
+        do {
+            NumStr += LastChar;
+            LastChar = getchar();
+        }while(isdigit(LastChar) || LastChar == '.');
+
+        NumVal = strtod(NumStr.c_str(), nullptr);
+        return tok_number;
+    }
+
+    if (LastChar == '#') {
+        // Comment until the end of the line
+        do {
+            LastChar = getchar();
+        } while(LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+        if (LastChar != EOF) {
+            return gettok();
+        }
+    }
+
+    // Check the end of the file
+    if (LastChar == EOF) {
+        return tok_eof;
+    }
+
+    // Otherwise just return the character as the ascii value
+    int ThisChar = LastChar;
+    LastChar = getchar();
+    return ThisChar;
+}
+
+/* Abstraction Syntax Tree --- AST, the parse tree*/
+namespace {
+
+// ExprAST, base class for all expression nodes
+class ExprAST {
+    public:
+    virtual ~ExprAST() = default;
+};
+
+// NumberExprAST - Expression class for number like '1.0'
+class NumberExprAST: public ExprAST {
+    double Val;
+
+    public:
+    NumberExprAST(double val):Val(val) {}
+};
+
+// VariableExprAST - Expression class for referecing a varible
+class VariableExprAST: public ExprAST {
+    string Name;
+
+    public:
+    VariableExprAST(const string &name): Name(name) {}
+};
+
+// BinaryExprAST - Expression class for binary operator
+class BinaryExprAST: public ExprAST {
+    char Op;
+    unique_ptr<ExprAST> LHS, RHS;
+
+    public:
+    BinaryExprAST(char Op, unique_ptr<ExprAST> LHS, unique_ptr<ExprAST> RHS):
+        Op(Op), LHS(move(LHS)), RHS(move(RHS)) {}
+};
+
+// CallExprAST - Expression class for function calls
+class CallExprAST: public ExprAST {
+    string Callee;
+    vector<unique_ptr<ExprAST>> Args;
+
+    public:
+    CallExprAST(const string &Callee, vector<unique_ptr<ExprAST>> Args): 
+        Callee(Callee), Args(move(Args)) {}
+};
+
+// PrototypeAST -- This class represents the "prototype" for a function,
+// which captures its name, and its argument names (thus implicitly the number
+// of arguments the function takes).
+class PrototypeAST {
+    string Name;
+    vector<string> Args;
+
+    public:
+    PrototypeAST(const string &Name, vector<string> Args):
+        Name(Name), Args(move(Args)) {}
+
+    const string &getName() const { return Name; }
+};
+
+// FunctionAST --- reprents a function definition
+class FunctionAST {
+    unique_ptr<PrototypeAST> Proto;
+    unique_ptr<ExprAST> Body;
+
+    public: 
+    FunctionAST(unique_ptr<PrototypeAST> Proto, unique_ptr<ExprAST> Body):
+        Proto(move(Proto)), Body(move(Body)) {}
+}; 
+
+} // end of namespace
+
+
+/* Parser */
+/// CurTok/getNextToken - Provide a simple token buffer.  CurTok is the current
+/// token the parser is looking at.  getNextToken reads another token from the
+/// lexer and updates CurTok with its results.
+static int CurTok;
+static int getNextToken() { return CurTok = gettok(); }
+
+// BinopPrecedence - the precedence for each binary operator token
+static map<char, int> BinopPrecedence;
+
+// GetTokPrecedence - get the precedence of the token from map
+static int GetTokPrecedence() {
+    if (!isascii(CurTok)) {
+        return -1;
+    }
+
+    int TokPrec = BinopPrecedence[CurTok];
+    if (TokPrec <= 0) {
+        return -1;
+    }
+    return TokPrec;
+}
+
+/// LogError* - These are little helper functions for error handling.
+unique_ptr<ExprAST> LogError(const char *Str) {
+  fprintf(stderr, "Error: %s\n", Str);
+  return nullptr;
+}
+unique_ptr<PrototypeAST> LogErrorP(const char *Str) {
+  LogError(Str);
+  return nullptr;
+}
+
+static unique_ptr<ExprAST> ParseExpression();
+
+/// numberexpr ::= number
+static unique_ptr<ExprAST> ParseNumberExpr() {
+    auto res = make_unique<NumberExprAST>(NumVal);
+    getNextToken();
+    return move(res);
+}
+
+/// parenexpr ::= '(' expression ')'
+static unique_ptr<ExprAST> ParseParenExpr() {
+    getNextToken(); // eat (
+    auto v = ParseExpression();
+
+    if (!v) {
+        return nullptr;
+    }
+
+    if (CurTok != ')') {
+        return LogError("expected ')'");
+    }
+
+    getNextToken(); // eat )
+    return v;
+}
+
+/// identifierexpr
+///   ::= identifier
+///   ::= identifier '(' expression* ')'
+static unique_ptr<ExprAST> ParseIdentifierExpr() {
+    string IdName = IdentifierStr;
+
+    getNextToken(); // eat IdentifierStr
+    if (CurTok != '(') {
+        return make_unique<VariableExprAST>(IdName);
+    }
+
+    // Call
+    getNextToken(); // eat  (
+    vector<unique_ptr<ExprAST>> Args;
+    if (CurTok != ')') {
+        while(true) {
+            if (auto Arg = ParseExpression()) {
+                Args.push_back(move(Arg));
+            }
+            else {
+                return nullptr;
+            }
+
+            if (CurTok == ')') {
+                break;
+            }
+
+            if (CurTok != ',') {
+                return LogError("Expected ')' or ',' in argument list");
+            }
+
+            getNextToken(); 
+        }
+    }
+
+    getNextToken(); // eat ')'
+
+    return make_unique<CallExprAST>(IdName, move(Args));
+}
+
+/// primary
+///   ::= identifierexpr
+///   ::= numberexpr
+///   ::= parenexpr
+static unique_ptr<ExprAST> ParsePrimary() {
+    switch(CurTok) {
+        default: 
+            return LogError("unknown token when expecting an expression");
+        case tok_identifier:
+            return ParseIdentifierExpr();
+        case tok_number:
+            return ParseNumberExpr();
+        case '(':
+            return ParseParenExpr();
+    }
+}
+
+/// binoprhs
+///   ::= ('+' primary)*
+static unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec, unique_ptr<ExprAST> LHS) {
+    //if binop, find precedence
+    while(1) {
+        int TokPrec = GetTokPrecedence();
+
+        if (TokPrec < ExprPrec) {
+            return LHS;
+        }
+
+        int BinOp = CurTok;
+        getNextToken(); // eat the binop
+
+        // Parse the primary expression after binop
+        auto RHS = ParsePrimary();
+        if (!RHS) {
+            return nullptr;
+        }
+
+        int NextPrec = GetTokPrecedence();
+        if (TokPrec < NextPrec) {
+            RHS = ParseBinOpRHS(TokPrec + 1, move(RHS));
+            if (!RHS) {
+                return nullptr;
+            }
+        }
+
+        // Merge LHS, RHS
+        LHS = make_unique<BinaryExprAST>(BinOp, move(LHS), move(RHS));
+
+    }
+}
+
+/// expression
+///   ::= primary binoprhs
+///
+
+static void MainLoop() {
+    // TODO
+}
+
+int main() {
+
+    BinopPrecedence['<'] = 10;
+    BinopPrecedence['+'] = 20;
+    BinopPrecedence['-'] = 20;
+    BinopPrecedence['*'] = 40;
+
+     // Prime the first token.
+    fprintf(stderr, "ready> ");
+    getNextToken();
+
+    // Run the main "interpreter loop" now.
+    MainLoop();
+
+    return 0;
+}
